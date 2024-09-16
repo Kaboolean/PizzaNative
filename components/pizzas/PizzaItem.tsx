6cd8755aa378554db6dfe930bce7fda159e1a30e
Florian Lernould <florianlernould7@gmail.com>
2024-09-16 16:55:08 +0200

    Create pizza component & start glow up css

---
+++ components/pizzas/PizzaItem.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { View, Text, StyleSheet, Image} from 'react-native';
+
+type Props = {
+    name: string;
+    price: number;
+    image: string;
+}
+
+const PizzaItem = (props: Props) => {
+    //Return an image's path placed in the assets/images/ folder 
+    function getImage(image: string): string{
+        return "../../assets/images/" + image;
+    }
+
+    return (
+        <View style={styles.card}>
+            <View>
+                <Image
+                    source={{uri: getImage(props.image)}}
+                    style={styles.image}
+                />;
+            </View>
+            <View>
+                <Text>Pizza: {props.name}!</Text>
+                <Text>Prix: {props.price}€</Text>
+            </View>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    card: {
+        flex: 1,
+        margin: 10,
+        backgroundColor: '#fff',
+        borderRadius: 10,
+        shadowColor: '#000',
+        shadowOffset: {
+          width: 0,
+          height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,   
+    
+        elevation: 5,
+      },
+    image: {
+        width: "25%",
+        height: '25%',
+    }
+});
+
+export default PizzaItem;
