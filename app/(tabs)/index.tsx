e78e47e194cfcb7ef8264f3114ccf9c79c417413
Demdoum Yanis <demdoum.yanis@hotmail.com>
2024-09-16 17:05:33 +0200

    create new tabs productList Component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,76 +1,42 @@
-import React, { useEffect, useState } from 'react';
-import { Image, StyleSheet, Platform, View, Text, ActivityIndicator } from 'react-native';
+import { Image, StyleSheet, Platform } from 'react-native';
 
 import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
-import ApiType from '../api';
-
+import ProductList from './productList';
+import { View } from 'react-native-reanimated/lib/typescript/Animated';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Products } from './productList';
 export default function HomeScreen() {
-  const [pizzas, setPizzas] = useState([]);
-  const api = new ApiType()
-
-  useEffect(() => {
+  const [data, setData] = useState<Products>([]);
     
-     const getPizzas = async () => {
-      try {
-        const response = await api.pizzas.getPizzas()
-        if (!response) {
-          throw new Error('Erreur récupération pizzas');
+
+    useEffect(() => {
+      const fetchData = async () => {
+        try {
+          const response = await axios.get('url_api');
+          // parse data wirh Products type
+          
+          const data = response.data
+          setData(response.data);
+        } catch (error) {
+          console.error(error);
         }
-        setPizzas(response)
-      } catch (error: any) {
-        console.log(error);
-      }
-    };
+      };
 
-    getPizzas();
-
-  }, []);
+      fetchData();
+    }, []);
+  
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }
-    >
-          <View style={styles.container}>
-      {pizzas.length === 0 ? (
-        <Text>No pizzas available.</Text>
-      ) : (
-        pizzas.map((pizza, index) => (
-          <Text key={index}>{pizza.name}</Text>
-        ))
-      )}
-    </View>
-    </ParallaxScrollView>
+   
+    <View>
+      
+        <ProductList Products={data} ></ProductList>
+       
+        </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-});
+
